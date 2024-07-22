91ae0b1ca61f47e6172aff16d22932c2f78519e6
Guilherme <guilhermesoaresilvat@gmail.com>
2024-07-22 15:04:57 -0300

    Projeto Back-End da FarmaMais

---
+++ src/main/java/com/generation/drogamais/DrogamaisApplication.java
@@ -0,0 +1,13 @@
+package com.generation.drogamais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrogamaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrogamaisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/drogamais/DrogamaisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.drogamais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrogamaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
